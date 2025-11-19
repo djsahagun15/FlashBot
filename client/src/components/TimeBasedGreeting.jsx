a98cc83e737ffa0c64833e19fdbc9d84894d8b7c
djsahagun15 <179677237+djsahagun15@users.noreply.github.com>
2025-11-19 15:10:42 +0800

    Fix time-based greeting

--- client/src/components/TimeBasedGreeting.jsx
+++ client/src/components/TimeBasedGreeting.jsx
@@ -1,9 +1,10 @@
 export default function TimeBasedGreeting() {
     const getGreeting = () => {
         const hour = new Date().getHours();
+        console.log(hour);
 
         if (hour >= 5 && hour < 12) return "Good Morning";
-        else if (hour >= 12 && hour < 5) return "Good Afternoon";
+        else if (hour >= 12 && hour < 17) return "Good Afternoon";
         else return "Good Evening";
     }
 
