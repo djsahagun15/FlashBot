3463c83a99112d35e5dc1c919484ae0cb504bd34
djsahagun15 <179677237+djsahagun15@users.noreply.github.com>
2025-11-19 15:12:39 +0800

    Remove unnecessary console logging

--- client/src/components/TimeBasedGreeting.jsx
+++ client/src/components/TimeBasedGreeting.jsx
@@ -1,7 +1,6 @@
 export default function TimeBasedGreeting() {
     const getGreeting = () => {
         const hour = new Date().getHours();
-        console.log(hour);
 
         if (hour >= 5 && hour < 12) return "Good Morning";
         else if (hour >= 12 && hour < 17) return "Good Afternoon";
